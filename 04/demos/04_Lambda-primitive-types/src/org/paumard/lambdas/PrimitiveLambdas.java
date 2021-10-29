1c549a2c3b4b558c97ff55f8ded7af9da7c2e2c2
KatieGott <Katie.Gott@target.com>
2021-10-29 17:19:11 -0500

    completed 4th module

--- 04/demos/04_Lambda-primitive-types/src/org/paumard/lambdas/PrimitiveLambdas.java
+++ 04/demos/04_Lambda-primitive-types/src/org/paumard/lambdas/PrimitiveLambdas.java
@@ -17,3 +17,21 @@ public class PrimitiveLambdas {
 		
 	}
 }
+
+/*
+Lambdas are not instances of anonymous classes!
+What can be written in a lambda can also be written in an anonymous class, but they are not the same thing.
+The compiled code is very different. Lambdas used a specific kind of byte code called invokedynamic. Performance
+is VERY different. (60x better)
+Avoid autoboxing (automatic conversion from a primitive type to an Object).
+Autounboxing(removes wrapper and returns primitive type).
+Example:
+Comparator<Integer> cmp = (i1, i2) -> Integer.compare(i1, i2);
+
+int compared = cmp.compare(10, 20);
+values here are passed in as primitive types, which means that the comparator (which takes an object), needs
+to box them into Integer, and then unbox them to run the compared function as int (primitive type). This
+comes at a cost. Be aware of this and choose another way of doing this.
+Use java.util specific version (like IntPredicate, IntFunction<T>) that will not need to box and then unbox.
+It takes in a primitive type.
+ */
